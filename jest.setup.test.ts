42aa51416ea112efa2be52e7d3a22ed45752caf3
Jack Steam <jacksteamdev@gmail.com>
2020-12-21 16:19:02 -0600

    tests: add webextension-polyfill

--- jest.setup.test.ts
+++ jest.setup.test.ts
@@ -1,3 +1,9 @@
+test('browser is defined in the global scope', () => {
+  expect(browser).toBeDefined()
+  expect(browser.runtime).toBeDefined()
+  expect(browser.runtime.sendMessage).toBeDefined()
+})
+
 test('chrome is mocked in the global scope', () => {
   expect(chrome).toBeDefined()
   expect(chrome.runtime).toBeDefined()
