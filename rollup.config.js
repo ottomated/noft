bda94ca806bbce8badc4caa2be695d57d528ec03
Jack Steam <jacksteamdev@gmail.com>
2021-04-21 10:21:49 -0500

    fix: remove esm react package

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,12 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
-import alias from '@rollup/plugin-alias'
 
 import { chromeExtension, simpleReloader } from 'rollup-plugin-chrome-extension'
 import { emptyDir } from 'rollup-plugin-empty-dir'
 import zip from 'rollup-plugin-zip'
 
 const isProduction = process.env.NODE_ENV === 'production'
-
-// Aliases for module resolution
-const aliases = isProduction
-  ? [
-      {
-        find: 'react',
-        // Use the production build
-        replacement: require.resolve('react/esm/react.production.min.js'),
-      },
-      {
-        find: 'react-dom',
-        // Use the production build
-        replacement: require.resolve(
-          'react-dom/esm/react-dom.production.min.js',
-        ),
-      },
-    ]
-  : []
 
 export default {
   input: 'src/manifest.json',
@@ -38,7 +19,6 @@ export default {
     chromeExtension(),
     // Adds a Chrome extension reloader during watch mode
     simpleReloader(),
-    alias({ entries: aliases }),
     resolve(),
     commonjs(),
     typescript(),
